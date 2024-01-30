77697e5d7e1ea853d62f7ca393c47056c03a0089
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-01-30 16:43:06 +0600

    3rd java code will be displayed here

---
+++ hello2.java
@@ -0,0 +1,11 @@
+public class hello2 {
+    
+        public static void main(String [] arguments ) {
+        System.out.println("Hello Wrold");
+        // printing java line 
+        System.out.println("Line Number 2");
+        // java line number 2 will be displayed here 
+        }
+        
+        
+}
