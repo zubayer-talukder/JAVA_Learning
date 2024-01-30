54e3874f5760bc5f11fe725a3b5aa7497a058cb6
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-01-30 16:39:13 +0600

    2nd java code

---
+++ javmsg.java
@@ -0,0 +1,9 @@
+public class javmsg {
+    
+        public static void  main(String [] arguments ){
+        //Statements
+        System.out.println("OUTPUT  sending Message ");
+        // string  will be here
+         
+        }
+}
