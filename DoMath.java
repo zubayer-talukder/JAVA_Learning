b53ad0e26527537399316c12b912d3a92fbe6b7e
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-01-30 16:54:02 +0600

    MAth Expression

---
+++ DoMath.java
@@ -0,0 +1,8 @@
+public class DoMath {
+    
+        public static void main(String [] argumrents){
+        double score= 10.2+5*0.4;
+        System.out.println(score);
+        score=score/5.2;
+        System.out.println(score);}}
+
