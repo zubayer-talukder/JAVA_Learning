c75074e7646d41d4c2a770bba6a331e04846792b
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-01-30 16:51:59 +0600

    Data Type and variables

---
+++ dataTypeAndVariable.java
@@ -0,0 +1,25 @@
+public class dataTypeAndVariable {
+  
+        public static void main (String [] arguments){
+        // variable_Data_type give_a_name
+         int foo;
+        foo=324;
+        //integer variable  decleration
+        System.out.println(foo);
+
+        double bad_pi=3.1416;
+        System.out.println(bad_pi);
+
+        // double variable decleration
+        boolean is_january ;
+        // boolean variable decleration
+
+        String word =("Joy Bangla Joy bongabondhu ");
+        // word assignment into a strings method 1
+        System.out.println(word);
+        word ="Something else ";
+            // word assignment into a strings method 2
+        System.out.println(word);
+        
+        } 
+}
