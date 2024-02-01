58319678a088710d05a4f14de570a858cfcb307e
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-02-01 06:49:12 +0600

    UPdated on variables

---
+++ primitiveVariable.java
@@ -0,0 +1,5 @@
+public class primitiveVariable {
+    public static void main(String[] args){
+
+    }
+}
---
+++ variableS.java
@@ -0,0 +1,9 @@
+public class variableS {
+    public static void main(String[]args){
+    int age = 32;
+    System.out.println(age);
+    int herAge=89;
+    age=56;
+    System.out.println(herAge+" "+ age);
+}
+}
