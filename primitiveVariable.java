003328a463bf95c36d17bb51a27e9971d20cfa68
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-02-01 07:09:02 +0600

    Update 1.1

--- primitiveVariable.java
+++ primitiveVariable.java
@@ -3,6 +3,8 @@ public class primitiveVariable {
 byte age =90;
 long viewsCount=4224_42_342L;
 float price = 45.99F;
-
+char letter = 'Z';
+System.out.println(age);
+System.out.println(viewsCount);
     }
 }
