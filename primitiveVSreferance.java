42ba35a7e10deabd9845fb9c1d22d22b17d051f5
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-02-02 18:55:02 +0600

    ref type

--- primitiveVSreferance.java
+++ primitiveVSreferance.java
@@ -1,6 +1,23 @@
-public class primitiveVSreferance {
-    public static void main() {
-        byte x = 1;
 
+import java.awt.*;
+public class primitiveVSreferance {
+    public static void main(String[]args) {
+       // primitive type variable
+
+       /* 
+
+        byte x=3;
+        byte y=x;
+        x=2;
+        System.out.println(y);
+
+        */
+         //Referance Type
+         Point point1 =new Point(1 ,2);
+         Point point2=point1;
+         point1.x=22;
+         System.out.println(point2);
+
+ 
     }
 }
