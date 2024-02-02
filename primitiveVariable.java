b41908460d9d6d2f969bc776d7db764d6a771e96
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-02-02 11:41:52 +0600

    Primitive Variable Updated

--- primitiveVariable.java
+++ primitiveVariable.java
@@ -2,7 +2,10 @@ public class primitiveVariable {
     public static void main(String[] args){
 byte age =90;
 long viewsCount=4224_42_3_3224_42L;
+//long long e variable er ses e "L" change kore dite hobe
 float price = 45.99F;
+//float variable er ses e akta "F" dite hobe
+
  char letter = 'Z';
  boolean isEngaged=true;
 System.out.println(age);
