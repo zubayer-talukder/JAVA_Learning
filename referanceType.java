2642a3dadecd3c0961b63f33f76373b4e14cb80f
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-02-02 11:55:23 +0600

    Referance Type

---
+++ referanceType.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class referanceType {
+    public static void main(String[]args){
+        Date now= new Date();
+        now.getTime();
+        // jehetu eita akta referance type variable tai '.' use korley automatic kaj kore fele 
+        
+        // now.getTime()--> eita diye auto date and Time paye jay
+        System.out.println("Abdullah Zubayer Talukder");
+        System.out.println(now);
+        //sout dile auto println hoye jay 
+    }
+}
