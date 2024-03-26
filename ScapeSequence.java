479c5eb041cfd9933e5a34900b8bcfdc0f3576a2
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-03-26 12:41:33 +0600

    Scape Sequence 5

--- ScapeSequence.java
+++ ScapeSequence.java
@@ -7,8 +7,12 @@ public class ScapeSequence {
         // \\ -->> this will display once
         String message3 = "C:\\ \n Windows\\ ";
         // \n -->> this create a new line
+
+        String message4= "C:\t Windows||";
         System.out.println(message);
         System.out.println(message2);
+        System.out.println(message3);
+        System.out.println(message4);
     }
 
 }
