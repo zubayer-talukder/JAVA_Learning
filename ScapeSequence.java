6f45e07513259bc2b35a16bb66d772c69566db3a
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-03-26 12:37:24 +0600

    Scapesequence 4

--- ScapeSequence.java
+++ ScapeSequence.java
@@ -1,9 +1,14 @@
 public class ScapeSequence {
 
-    public static void main(String[] args){
-        String message= "Hello!! \"Zubayer\" ";
+    public static void main(String[] args) {
+        String message = "Hello!! \"Zubayer\" ";
         // " TEXT"--> this will scapesequence in java program
-        
+        String message2 = " C:\\Windows\\";
+        // \\ -->> this will display once
+        String message3 = "C:\\ \n Windows\\ ";
+        // \n -->> this create a new line
         System.out.println(message);
-    } 
+        System.out.println(message2);
+    }
+
 }
