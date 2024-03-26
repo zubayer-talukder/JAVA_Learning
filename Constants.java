544c2a8417ed1ced2131ee4bddcb37b17b8bae5a
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-03-26 19:16:24 +0600

    Constants

---
+++ Constants.java
@@ -0,0 +1,13 @@
+import javax.sound.midi.Soundbank;
+
+public class Constants {
+    public static void main(String[]args){
+        float pi =3.1416F;
+        System.out.println(pi);
+         final float PI=3.233F;
+        // IF fianl  value is added in before of any variable it can not be changed at once
+         System.out.println(PI);
+
+
+    }
+}
