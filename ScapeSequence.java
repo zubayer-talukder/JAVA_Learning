031729ea9956930d6e01dd48c1f0bad17c9a80d1
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-03-26 12:29:38 +0600

    Scape Sequence 2

--- ScapeSequence.java
+++ ScapeSequence.java
@@ -1,7 +1,9 @@
 public class ScapeSequence {
 
     public static void main(String[] args){
-        String message= "Hello!! Zubayer";
+        String message= "Hello!! \"Zubayer\" ";
+        // " TEXT"--> this will scapesequence in java program
+        
         System.out.println(message);
     } 
 }
