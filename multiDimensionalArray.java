d6d13ffb3281916aea5e1e86da69ee40497f655f
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-03-26 19:05:52 +0600

    Arrays

--- multiDimensionalArray.java
+++ multiDimensionalArray.java
@@ -10,11 +10,13 @@ public class multiDimensionalArray {
         System.out.println(Arrays.deepToString(TwoDArray));
         // this is a Two Dimensional Array Example.
 
-        // Another way to Express multidimensional Array method
         int [][][] threeDimensional =new int [2][3][4];
         threeDimensional[0][2][1]=74;
         System.out.println(Arrays.deepToString(threeDimensional));
 
+        // Another way to Express multidimensional Array method
+       int [][]Newpapers={{10,20,54,14},{41,47}};
+System.out.println(Arrays.deepToString(Newpapers));
 
     }
 }
