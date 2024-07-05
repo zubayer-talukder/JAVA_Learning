a81939d5b64072d0508928cdb0b36cd49cb8bb69
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-07-05 12:54:06 +0600

    oop

--- oop.java
+++ oop.java
@@ -1,28 +1,43 @@
-class Pen {
-    String color;
-    String types;
+// class Pen {
+//     String color;
+//     String types;
 
-    public void write() {
-        System.out.println("Writing Something");
+//     public void write() {
+//         System.out.println("Writing Something");
+//     }
+
+//     public void printColor() {
+//         System.out.println(this.color);
+//     }
+// }
+
+class Student{
+    String name;
+    int age;
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
     }
 
-    public void printColor() {
-        System.out.println(this.color);
-    }
 }
 
 public class oop {
     public static void main(String[] args) {
-        Pen pen1 = new Pen();
-        pen1.color = "Blue";
-        pen1.types = "Gel";
-        pen1.write();
-        pen1.printColor();
+        // Pen pen1 = new Pen();
+        // pen1.color = "Blue";
+        // pen1.types = "Gel";
+        // pen1.write();
+        // pen1.printColor();
 
-        Pen pen2 = new Pen();
-        pen2.color = "Black";
-        pen2.types = "BallPoint";
-        pen2.write();
-        pen2.printColor();
+        // Pen pen2 = new Pen();
+        // pen2.color = "Black";
+        // pen2.types = "BallPoint";
+        // pen2.write();
+        // pen2.printColor();
+
+        Student s1 = new Student();
+        s1.name="Zubayer";
+        s1.age=22;
+        s1.printInfo();
     }
 }
